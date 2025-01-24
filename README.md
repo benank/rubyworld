d2624050706edb3dd77eaba8a9fceadbd03e98e8
benank <gh@benank.com>
2025-01-24 11:06:49 -1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ruby World
+
+Realtime online 2D game world. Built with Cloudflare Durable Objects & Pages, React, and Typescript. Inspired by Pokémon Ruby.
+
---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import viteLogo from '/ruby.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1>Ruby World</h1>
+      <p>Coming soon</p>
+    </>
+  )
+}
+
+export default App
